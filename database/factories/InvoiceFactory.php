719cb17b18fad23d9733aba32c389bacc89232af
Celina Ferreira Ribeiro <ribeiro.f.celina@gmail.com>
2023-11-08 11:17:28 -0300

    Project, migration and factory

---
+++ database/factories/InvoiceFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use App\Models\Invoice;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+use function PHPSTORM_META\type;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Invoice>
+ */
+class InvoiceFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $paid = $this->faker->boolean();
+
+        return [
+            'user_id' => User::all()->random(),
+            'type' => $this->faker->randomElement(['B', 'C', 'P']),
+            'paid' => $paid,  
+            'value' => $this->faker->numberBetween(1000, 10000),
+            'payment_date' => $paid ? $this->faker->randomElement([$this->faker->dateTimeThisMonth()]) : NULL
+        ];
+    }
+}
